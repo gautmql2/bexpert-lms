a02c6439634c0e29201aff3e64e653873e871b9e
gautmql2 <gautmql2@gmail.com>
2025-08-03 00:14:53 +0530

    Add files via upload

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,31 +1,14 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = "/";
-  } else {
-    const subscribed = localStorage.getItem("hasSubscribedToBexpert") === "true";
-    const confirmed = sessionStorage.getItem("hasSubscribedToBexpert") === "true";
-
-    const isOnSubscribePage = window.location.pathname.includes("subscribe.html");
-    if (!subscribed && !confirmed && !isOnSubscribePage) {
-      window.location.href = "/subscribe.html";
-    }
-  }
-});
-
-export { auth };
+export const auth = getAuth(app);
