68ca41667566f33b2738954b960403afdfae1641
gautmql2 <gautmql2@gmail.com>
2025-08-03 00:09:25 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBCoL3LFhEuSFQPWJphgCMERJ7skl_YPjs",
+  authDomain: "bexpert-lms.firebaseapp.com",
+  projectId: "bexpert-lms",
+  storageBucket: "bexpert-lms.firebasestorage.app",
+  messagingSenderId: "341450782292",
+  appId: "1:341450782292:web:4ee4725d0c6b23496f9fc1"
+};
