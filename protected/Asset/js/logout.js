d68c83c1826e2e645b3d5073ad343c1ad26ce7b7
gautmql2 <gautmql2@gmail.com>
2025-08-03 05:53:42 +0530

    Update logout.js

--- protected/Asset/js/logout.js
+++ protected/Asset/js/logout.js
@@ -1,16 +1,15 @@
-// logout.js
 import { signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { auth } from "/firebase-auth.js";
 
-window.logout = function () {
+window.logout = function() {
   signOut(auth)
     .then(() => {
       localStorage.removeItem("hasSubscribedToBexpert");
       sessionStorage.clear();
       window.location.href = "/login.html";
     })
-    .catch((error) => {
-      console.error("Logout error:", error);
+    .catch(err => {
+      console.error("Logout error", err);
       alert("Logout failed. Please try again.");
     });
-};
+}
