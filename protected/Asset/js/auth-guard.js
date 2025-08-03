97993421c80b40163dc7872967a599db4370bc2b
gautmql2 <gautmql2@gmail.com>
2025-08-03 06:11:42 +0530

    Delete protected/Asset directory

--- protected/Asset/js/auth-guard.js
+++
@@ -1,8 +0,0 @@
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { auth } from "/firebase-auth.js";
-
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = "/login.html";
-  }
-});
